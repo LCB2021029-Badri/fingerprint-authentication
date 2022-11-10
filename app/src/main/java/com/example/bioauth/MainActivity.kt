d593c5c49bc02137ac28f83d065347c17827c27f
badri_akkala <lcb2021029@iiitl.ac.in>
2022-11-11 01:15:56 +0530

    step 2 : what to do for auth success and auth fail

--- app/src/main/java/com/example/bioauth/MainActivity.kt
+++ app/src/main/java/com/example/bioauth/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.bioauth
 
+import android.content.Intent
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CancellationSignal
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+
+    private var cancellationSignal: CancellationSignal? = null
+
+    private val authenticationCallback: BiometricPrompt.AuthenticationCallback get() =
+        @RequiresApi(Build.VERSION_CODES.P)
+        object : BiometricPrompt.AuthenticationCallback(){
+            //if auth failed
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                super.onAuthenticationError(errorCode, errString)
+                notifyUser("Authentication error: $errString :(")
+            }
+            //if auth success
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                super.onAuthenticationSucceeded(result)
+                notifyUser("Authentication Success :)")
+                //what to do if success
+                startActivity(Intent(this@MainActivity,SecondActivity::class.java))
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    //just simple toast
+    private fun notifyUser(message:String){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+    }
+
 }
