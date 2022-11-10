450f0bf6e63e70f9fb0ff38d2f1c41a8d17c4160
badri_akkala <lcb2021029@iiitl.ac.in>
2022-11-11 01:31:16 +0530

    step 3 : checking biometric support in the device and setting cancel signal

--- app/src/main/java/com/example/bioauth/MainActivity.kt
+++ app/src/main/java/com/example/bioauth/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.bioauth
 
+import android.app.KeyguardManager
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.hardware.biometrics.BiometricPrompt
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +11,7 @@ import android.os.Bundle
 import android.os.CancellationSignal
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,6 +37,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //checking of biometeric eists in the system or not
+        checkBiometricSupport()
+    }
+
+    //checking boimetric support
+    private fun checkBiometricSupport():Boolean{
+        val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        if(keyguardManager.isKeyguardSecure){
+            notifyUser("FingePrint authentication is not enabled")
+            return false
+        }
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.USE_BIOMETRIC) != PackageManager.PERMISSION_GRANTED){
+            notifyUser("Fingerprint authentication permission is not enabled")
+            return false
+        }
+        if(packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)){
+            return true
+        }
+        else{
+            return true
+        }
+    }
+
+    //on cancelled
+    private fun onCancellationSignal() : CancellationSignal{
+        cancellationSignal = CancellationSignal()
+        cancellationSignal?.setOnCancelListener {
+            notifyUser("Authentication was cancelled by the user")
+        }
     }
 
     //just simple toast
